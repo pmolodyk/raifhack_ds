fde63c40317de8e40655b0f84cdc1591eec28600
Artem Alekberov <art591>
2021-09-25 01:34:04 +0300

    change pipeline

--- raifhack_ds/features.py
+++ raifhack_ds/features.py
@@ -1,13 +1,58 @@
 import pandas as pd
 from raifhack_ds.utils import UNKNOWN_VALUE
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, OrdinalEncoder
 
-def prepare_categorical(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Заполняет пропущенные категориальные переменные
-    :param df: dataframe, обучающая выборка
-    :return: dataframe
-    """
-    df_new = df.copy()
-    fillna_cols = ['region','city','street','realty_type']
-    df_new[fillna_cols] = df_new[fillna_cols].fillna(UNKNOWN_VALUE)
-    return df_new
+
+
+class FeaturesPreparation:
+    def __init__(self, config):
+        self.config = config
+        self.categorical_features = config.get('categorical_features')
+
+    def prepare_train(d) -> pd.DataFrame:
+        df_new = df.copy()
+        
+        
+        # add nans
+        replace_nans = config.get('replace_nans', 'unknown_value')
+        for col in df_new.columns():
+            if df_new[col].isna().sum() != 0:
+                if col in self.categorical_features:
+                    df_new[col].fillna(UNKNOWN_VALUE)
+                elif col in self.numerical_features:
+                    df_new[col].fillna(df[!df_new[col].isna()].mean())
+
+        # features interaction
+        features_to_interact = self.config.get(features_to_interact, [])
+        for f1 in features_to_interact:
+            for f2 in features_to_interact:
+                if f1 != f2:
+                    df_new[f'{f1}_{f2}'] = df_new[f1] + df_new[f2] # ADD NORM
+
+
+        return df_new
+
+    def prepare_test(d) -> pd.DataFrame:
+        df_new = df.copy()
+        
+        
+        # add nans
+        replace_nans = config.get('replace_nans', 'unknown_value')
+        for col in df_new.columns():
+            if df_new[col].isna().sum() != 0:
+                if col in self.categorical_features:
+                    df_new[col].fillna(UNKNOWN_VALUE)
+                elif col in self.numerical_features:
+                    df_new[col].fillna(df[!df_new[col].isna()].mean())
+
+        # features interaction
+        features_to_interact = self.config.get(features_to_interact, [])
+        for f1 in features_to_interact:
+            for f2 in features_to_interact:
+                if f1 != f2:
+                    df_new[f'{f1}_{f2}'] = df_new[f1] + df_new[f2] # ADD NORM
+
+
+        return df_new
--- train.py
+++ train.py
@@ -2,12 +2,13 @@ import argparse
 import logging.config
 import pandas as pd
 from traceback import format_exc
+import json
 
 from raifhack_ds.model import BenchmarkModel
-from raifhack_ds.settings import MODEL_PARAMS, LOGGING_CONFIG, NUM_FEATURES, CATEGORICAL_OHE_FEATURES,CATEGORICAL_STE_FEATURES,TARGET
+from raifhack_ds.settings import LOGGING_CONFIG
 from raifhack_ds.utils import PriceTypeEnum
 from raifhack_ds.metrics import metrics_stat
-from raifhack_ds.features import prepare_categorical
+from raifhack_ds.features import FeaturesPreparation
 
 logging.config.dictConfig(LOGGING_CONFIG)
 logger = logging.getLogger(__name__)
@@ -30,7 +31,9 @@ def parse_args():
     parser.add_argument("--train_data", "-d", type=str, dest="d", required=True, help="Путь до обучающего датасета")
     parser.add_argument("--model_path", "-mp", type=str, dest="mp", required=True, help="Куда сохранить обученную ML модель")
     parser.add_argument("--val_data", "-v", type=str, dest="v", required=True, help="Путь до валидационного датасета")
-
+    parser.add_argument("--config", "-c", type=str, dest="v", required=True, help="Конфиг препроцесса")
+    parser.add_argument("--test_data", "-t", type=str, dest="d", required=True, help="Путь до отложенной выборки")
+    parser.add_argument("--output", "-o", type=str, dest="o", required=True, help="Путь до выходного файла")
     return parser.parse_args()
 
 if __name__ == "__main__":
@@ -42,18 +45,33 @@ if __name__ == "__main__":
         train_df = pd.read_csv(args['d'])
         logger.info('Load validation df')
         val_df = pd.read_csv(args['v'])
+        logger.info('Load Test df')
+        test_df = pd.read_csv(args['t'])
         logger.info(f'Input shape: {train_df.shape}')
-        train_df = prepare_categorical(train_df)
 
-        X_offer = train_df[train_df.price_type == PriceTypeEnum.OFFER_PRICE][NUM_FEATURES+CATEGORICAL_OHE_FEATURES+CATEGORICAL_STE_FEATURES]
+        with open(args[c]) as f:
+            config = json.load(f)
+
+        FP = FeaturesPreparation(config)
+
+        train_df = FP.prepare_train(train_df)
+        val_df = FP.prepare_test(val_df)
+        test_df = FP.prepare_test(test_df)
+
+        NUM_FEATURES = config.get('num_features')
+        CATEGORICAL_OHE = config.get('categorical_ohe')
+        CATEGORICAL_ORDINAL = config.get('categorical_ordinal')
+        CATEGORICAL_TOMODEL = config.get('categorical_tomodel')
+
+        X_offer = train_df[train_df.price_type == PriceTypeEnum.OFFER_PRICE][NUM_FEATURES+CATEGORICAL_OHE+CATEGORICAL_ORDINAL+CATEGORICAL_TOMODEL]
         y_offer = train_df[train_df.price_type == PriceTypeEnum.OFFER_PRICE][TARGET]
-        X_manual = train_df[train_df.price_type == PriceTypeEnum.MANUAL_PRICE][NUM_FEATURES+CATEGORICAL_OHE_FEATURES+CATEGORICAL_STE_FEATURES]
+        X_manual = train_df[train_df.price_type == PriceTypeEnum.MANUAL_PRICE][NUM_FEATURES+CATEGORICAL_OHE+CATEGORICAL_ORDINAL+CATEGORICAL_TOMODEL]
         y_manual = train_df[train_df.price_type == PriceTypeEnum.MANUAL_PRICE][TARGET]
         logger.info(f'X_offer {X_offer.shape}  y_offer {y_offer.shape}\tX_manual {X_manual.shape} y_manual {y_manual.shape}')
 
-        X_val_offer = val_df[val_df.price_type == PriceTypeEnum.OFFER_PRICE][NUM_FEATURES+CATEGORICAL_OHE_FEATURES+CATEGORICAL_STE_FEATURES]
+        X_val_offer = val_df[val_df.price_type == PriceTypeEnum.OFFER_PRICE][NUM_FEATURES+CATEGORICAL_OHE+CATEGORICAL_ORDINAL+CATEGORICAL_TOMODEL]
         y_val_offer = val_df[val_df.price_type == PriceTypeEnum.OFFER_PRICE][TARGET]
-        X_val_manual = val_df[val_df.price_type == PriceTypeEnum.MANUAL_PRICE][NUM_FEATURES + CATEGORICAL_OHE_FEATURES + CATEGORICAL_STE_FEATURES]
+        X_val_manual = val_df[val_df.price_type == PriceTypeEnum.MANUAL_PRICE][NUM_FEATURES+CATEGORICAL_OHE+CATEGORICAL_ORDINAL+CATEGORICAL_TOMODEL]
         y_val_manual = val_df[val_df.price_type == PriceTypeEnum.MANUAL_PRICE][TARGET]
 
         model = BenchmarkModel(numerical_features=NUM_FEATURES, ohe_categorical_features=CATEGORICAL_OHE_FEATURES,
@@ -71,7 +89,7 @@ if __name__ == "__main__":
         metrics = metrics_stat(y_manual.values, predictions_manual)
         logger.info(f'Metrics stat for training data with manual prices: {metrics}')
 
-        #VALIDATION
+        # VALIDATION
         predictions_val_offer = model.predict(X_val_offer)
         metrics_val = metrics_stat(y_val_offer.values, predictions_val_offer / (
                     1 + model.corr_coef))  # для обучающей выборки с ценами из объявлений смотрим качество без коэффициента
@@ -80,6 +98,12 @@ if __name__ == "__main__":
         predictions_val_manual = model.predict(X_val_manual)
         metrics_val = metrics_stat(y_val_manual.values, predictions_val_manual)
         logger.info(f'Metrics stat for validation data with manual prices: {metrics_val}')
+        
+        # TEST
+        logger.info('Predict')
+        test_df['per_square_meter_price'] = model.predict(test_df[NUM_FEATURES+CATEGORICAL_OHE+CATEGORICAL_ORDINAL+CATEGORICAL_TOMODEL])
+        logger.info('Save results')
+        test_df[['id','per_square_meter_price']].to_csv(args['o'], index=False)
 
 
     except Exception as e:
